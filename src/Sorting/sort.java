6a4d847a727d923eecf3bf1692295159b7ddd2b9
Reyaz Ahamd Bhat <reyazbhat1881@gmail.com>
2026-02-07 11:22:50 +0530

    strings

---
+++ src/Sorting/sort.java
@@ -0,0 +1,10 @@
+package src.Sorting;
+import java.util.Arrays;
+
+public class sort {
+    public static void main(String[] args) {
+        int nums = {2,4,5};
+        Arrays.sort(nums);
+        System.out.println(nums);
+    }
+}
---
+++ src/Strings/first.java
@@ -0,0 +1,14 @@
+package src.Strings;
+
+public class first {
+    public static void main(String[] args) {
+        int[] arr = {2,5,7,8,9,4};
+        String str = "Reyaz Ahmad";
+        // System.out.println(str);
+
+        String a = "reyaz";
+        System.out.println(a);
+        String b = "reyaz";
+
+    }   
+}
