9a5c03bfe2f44f1093c6a8874cf14f79e6405ab3
Reyaz Ahamd Bhat <reyazbhat1881@gmail.com>
2026-02-08 11:57:37 +0530

    String Methods

---
+++ src/Strings/methods.java
@@ -0,0 +1,13 @@
+package src.Strings;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class methods {
+    public static void main(String[] args) {
+        String n = "Reyaz";
+        System.out.println(Arrays.toString(n.toCharArray()));
+        System.out.println(n.toLowerCase());
+        System.out.println(n.indexOf('z'));
+    }
+}
