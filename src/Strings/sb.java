7b175fd91ecdcf526739b98b7a4ba261c3ecfee0
Reyaz Ahamd Bhat <reyazbhat1881@gmail.com>
2026-02-08 11:49:37 +0530

    string builder

---
+++ src/Strings/sb.java
@@ -0,0 +1,14 @@
+package src.Strings;
+
+public class sb {
+    public static void main(String[] args) {
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < 26; i++) {
+            char ch = (char)('a' + i);
+            builder.append(ch);
+        }
+        builder.reverse();
+        System.out.println(builder.toString());
+    }
+}
